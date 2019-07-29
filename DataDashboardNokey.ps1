21124c14bd07619b942a3c1e175f2d2ad97eabfd
Zach Burnside <akie9001@gmail.com>
2019-07-29 15:32:41 -0400

    Added warning messages and changed output

--- DataDashboardNokey.ps1
+++ DataDashboardNokey.ps1
@@ -1,6 +1,6 @@
-﻿function messageSlack ($channel,$metric,$value) {
+function messageSlack ($channel,$metric,$value) {
     $percentOrValue = '%'
-    if ($metric -eq 'BatchesPerSec')
+    if ($metric -eq 'PageLifeExpectancy')
         {$percentOrValue = ''}
     $token=''
     Send-SlackMessage -Token $token -Channel "$channel" -Text "The $metric is over the threshold. It is currently at $value$percentOrValue. Please resolve."
@@ -9,33 +9,44 @@
 function loadCPUData ($dbname, $serverName, $instanceName, $user, $password) {
     $CpuLoad = (Get-WmiObject win32_processor | Measure-Object -property LoadPercentage -Average | Select Average ).Average
     $violationBit=0
-    if ($CpuLoad -gt 50) {$violationBit=1}
+    $CpuThreshold = Invoke-sqlcmd -Query "select threshold from $dbname.dbo.Metrics where MetricName='CPU'" -ServerInstance "$serverName\$instanceName" -Username $user -Password $password
+    if ($CpuLoad -gt $CpuThreshold[0]) {$violationBit=1}
     Invoke-Sqlcmd -Query "insert into $dbname.dbo.CurrentMetric values (1,$CpuLoad,$violationBit)" -ServerInstance "$serverName\$instanceName" -Username $user -Password $password
 }
 
-function gatherDisplayData ($metricID, $serverName, $instanceName, $userName, $password) {
-    $Results = Invoke-Sqlcmd -Query "select CM.Value, CM.ViolationBit,  M.MetricName, C.ContactName from dbo.CurrentMetric CM inner join Metrics M on M.MetricID=CM.MetricId inner join Contacts C on M.ContactID=C.ContactID where CM.MetricID=$metricID"  -ServerInstance "$serverName\$instanceName" -Username $userName -Password $password 
+function gatherDisplayData ($dbname, $metricID, $serverName, $instanceName, $userName, $password) {
+    $Results = Invoke-Sqlcmd -Query "select CM.Value, CM.ViolationBit,  M.MetricName, C.ContactName 
+                                    from $dbname.dbo.CurrentMetric CM 
+                                    inner join $dbname.dbo.Metrics M on M.MetricID=CM.MetricId 
+                                    inner join $dbname.dbo.Contacts C on M.ContactID=C.ContactID 
+                                    where CM.MetricID=$metricID"  -ServerInstance "$serverName\$instanceName" -Username $userName -Password $password 
     $Metric = $Results[2]
     $Value = $Results[0]
-    echo "-$Metric-"
-    echo "$Value/n"
     #if the violation detection bit is set to true, message the sysadmin channel on slack
     if ($Results[1] -eq 1) {
         messageSlack -channel $Results[3] -metric $Results[2] -value $Results[0] > $null
+        $ErrorDate = Invoke-Sqlcmd -Query "select Date from $dbname.dbo.Violations where MetricID=$metricID" -ServerInstance "$serverName\$instanceName" -Username $user -Password $password
+        $ErrorDate=$ErrorDate[0]
+        Write-Host -ForegroundColor Red "- $Metric - Currently in violation beginning at $ErrorDate"
+        echo "$Value`n"
         }
+    else {
+        echo "- $Metric -"
+        echo "$Value`n"
+    }
 }
 
-function displayData ($dbname, $serverName, $instanceName, $user, $password) {
-    #run stored procedure that gathers all of the statistics for memory and batches
-    Invoke-Sqlcmd -Query "exec $dbname.[dbo].[GatherMetrics]" -ServerInstance "$serverName\$instanceName" -Username $user -Password $password
+function displayLiveData ($dbname, $serverName, $instanceName, $user, $password) {
+    #run stored procedure that gathers all of the statistics for memory and PLE
+    Invoke-Sqlcmd -Query "exec $dbname.[dbo].[GatherMetrics]" -ServerInstance "$serverName\$instanceName" -Username $user -Password $password -QueryTimeout 2
     #clear the shell to make the dashboard appear to refresh instantly
-    #clear
-    $numOfMetrics = Invoke-Sqlcmd -Query "select count(*) from dbo.CurrentMetric" -ServerInstance "$serverName\$instanceName" -Username $user -Password $password
+    clear
+    $numOfMetrics = Invoke-Sqlcmd -Query "select count(*) from $dbname.dbo.CurrentMetric" -ServerInstance "$serverName\$instanceName" -Username $user -Password $password
     $numOfMetricsCounter = [int]$numOfMetrics[0]
     while ($numOfMetricsCounter -gt 0)
         { 
-            gatherDisplayData $numOfMetricsCounter $serverName $instanceName -userName $user $password
-            $numOfMetricsCounter-- 
+            gatherDisplayData -dbname $dbname -metricID $numOfMetricsCounter -serverName $serverName -instanceName $instanceName -userName $user -password $password
+            $numOfMetricsCounter= $numOfMetricsCounter-1 
         }
     
     #truncate the current data table to make room for the next batch of data
@@ -55,9 +66,9 @@ $passwordInput = Read-Host -Prompt "Password: " -asSecureString
 $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwordInput)            
 $passwordPlain = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
-#continue to run the command, refreshing the data and alerting the user, sleeping for 10 seconds, and then refreshing
+#continue to run the command, refreshing the data and alerting the user, sleeping for 5 seconds, and then refreshing
 while (1 -eq 1) {
     loadCPUData -dbname $dbInput -servername $serverInput -instanceName $instanceInput -user $userInput -password $passwordPlain
-    displayData -dbname $dbInput -servername $serverInput -instanceName $instanceInput -user $userInput -password $passwordPlain
+    displayLiveData -dbname $dbInput -servername $serverInput -instanceName $instanceInput -user $userInput -password $passwordPlain
     Start-Sleep -Seconds 5
     }
